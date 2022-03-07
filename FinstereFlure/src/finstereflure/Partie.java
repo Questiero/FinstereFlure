8d37630704c9bdac538086638332490b25508431
Questiero <19290903+Questiero@users.noreply.github.com>
2022-03-07 16:20:31 +0100

    Oui ?

--- FinstereFlure/src/finstereflure/Partie.java
+++ FinstereFlure/src/finstereflure/Partie.java
@@ -6,23 +6,21 @@ import finstereflure.players.Player;
  * Gestion d'une partie de jeu
  */
 public class Partie {
-    
+
     private final Host host;
-    
+
     private Player[] players = new Player[2];
     private Terrain map;
-    
+
     private boolean advancedMode = false;
-    
+
     /**
      * Constructeur de Partie en fonction de son hôte
-     * 
+     *
      * @param host Hôte de la partie
      */
     public Partie(Host host) {
         this.host = host;
     }
-    
-    
-    
+
 }
