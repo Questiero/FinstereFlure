d46682f005941d882e1be5b4ae6286bb84121122
Questiero <19290903+Questiero@users.noreply.github.com>
2022-03-07 16:17:55 +0100

    Host placeholder

--- FinstereFlure/src/finstereflure/Host.java
+++ FinstereFlure/src/finstereflure/Host.java
@@ -1,7 +1,19 @@
 package finstereflure;
 
 public class Host {
-    
-    
-    
+
+    private String name;
+
+    public Host(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
