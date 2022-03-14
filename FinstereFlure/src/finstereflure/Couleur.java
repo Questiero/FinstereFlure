588c366d1c9325e574c19717c64d33ff2fbc513a
Questiero <19290903+Questiero@users.noreply.github.com>
2022-03-14 10:51:03 +0100

    getType

--- FinstereFlure/src/finstereflure/Couleur.java
+++ FinstereFlure/src/finstereflure/Couleur.java
@@ -11,11 +11,11 @@ public enum Couleur {
     PURPLE,
     RED,
     YELLOW;
-    
+
     @Override
     public String toString() {
-        
-        switch(this) {
+
+        switch (this) {
             case BLUE:
                 return "bleu";
             case BROWN:
@@ -31,9 +31,50 @@ public enum Couleur {
             case YELLOW:
                 return "jaune";
         }
-        
+
         return null;
-        
+
     }
-    
+
+    public Couleur getType(String s) {
+
+        Couleur color = null;
+
+        s = s.toLowerCase();
+        s = s.strip();
+
+        switch (s) {
+
+            case "bleu":
+            case "blue":
+                color = Couleur.BLUE;
+                break;
+            case "brun":
+            case "brown":
+                color = Couleur.BROWN;
+                break;
+            case "gris":
+            case "gray":
+            case "grey":
+                color = Couleur.GRAY;
+                break;
+            case "vert":
+            case "green":
+                color = Couleur.GREEN;
+                break;
+            case "violet":
+            case "purple":
+                color = Couleur.PURPLE;
+                break;
+            case "jaune":
+            case "yellow":
+                color = Couleur.YELLOW;
+                break;
+
+        }
+
+        return color;
+
+    }
+
 }
--- FinstereFlure/src/finstereflure/players/PlayerType.java
+++ FinstereFlure/src/finstereflure/players/PlayerType.java
@@ -6,4 +6,25 @@ package finstereflure.players;
 public enum PlayerType {
     HUMAN,
     BOB;
+
+    public PlayerType getType(String s) {
+
+        PlayerType type = null;
+
+        s = s.toLowerCase();
+        s = s.strip();
+
+        switch (s) {
+            case "human":
+                type = PlayerType.HUMAN;
+                break;
+            case "bob":
+                type = PlayerType.BOB;
+                break;
+        }
+
+        return type;
+
+    }
+
 }
