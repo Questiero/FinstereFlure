582453711459409178e9b4d221182e662b4db1e3
Amandine <91495212+6prine@users.noreply.github.com>
2022-04-06 17:39:02 +0200

    début targetdirection monstre

--- FinstereFlure/src/finstereflure/pions/Monstre.java
+++ FinstereFlure/src/finstereflure/pions/Monstre.java
@@ -2,7 +2,12 @@ package finstereflure.pions;
 
 import finstereflure.Terrain;
 import finstereflure.enums.Direction;
+import static finstereflure.enums.Direction.DOWN;
+import static finstereflure.enums.Direction.LEFT;
+import static finstereflure.enums.Direction.RIGHT;
+import static finstereflure.enums.Direction.UP;
 import finstereflure.pions.interfaces.Moveable;
+import java.util.LinkedList;
 import javax.swing.ImageIcon;
 
 /**
@@ -33,27 +38,27 @@ public final class Monstre extends Pion implements Moveable {
         switch (dir) {
             case UP:
                 if (this.getY() == 0) {
-                    this.setY(15);
+                    this.setY(10);
                 } else {
                     this.setY(getY() - 1);
                 }
                 break;
             case LEFT:
                 if (this.getX() == 0) {
-                    this.setX(11);
+                    this.setX(15);
                 } else {
                     this.setX(getX() - 1);
                 }
                 break;
             case RIGHT:
-                if (this.getX() == 11) {
+                if (this.getX() == 15) {
                     this.setX(0);
                 } else {
                     this.setX(getX() + 1);
                 }
                 break;
             case DOWN:
-                if (this.getY() == 15) {
+                if (this.getY() == 10) {
                     this.setY(0);
                 } else {
                     this.setY(getY() + 1);
@@ -70,6 +75,66 @@ public final class Monstre extends Pion implements Moveable {
     @Override
     public boolean canMove(Direction dir) {
         return true;
+    }
+
+    public Direction targetDirection() {
+        //il faut récup les infos dans les 4 directions (joueur, pierre ou rien)
+        //ensuite on les compare et on return la direction la + opti
+        //si aucune direction opti : le monstre continue d'avancer dans sa direction précedante
+        //penser à actualiser l'image en fonction de la direction
+
+        LinkedList<Pion>[][] pionmap = new LinkedList[16][11];
+        pionmap = super.terrain.getPionMap();
+
+        Pion nextPosUP = pionmap[this.getX()][this.getY() - 1].getLast();
+        Pion nextPosLEFT = pionmap[this.getX() - 1][this.getY()].getLast();
+        Pion nextPosRIGHT = pionmap[this.getX() + 1][this.getY()].getLast();
+        Pion nextPosDOWN = pionmap[this.getX()][this.getY() + 1].getLast();
+
+        Pion[] tabNextPos = new Pion[]{nextPosUP, nextPosLEFT, nextPosRIGHT, nextPosDOWN};
+
+        boolean found = false;  //passe à vrai si on trouve un pion différent de Empty
+        int indiceDirection = 0;
+
+        boolean isSmthArround = false;  //test si on agrandit la recherche
+
+        while (!isSmthArround) {
+            
+            nextPosUP = pionmap[this.getX()][this.getY() - 1].getLast();
+        nextPosLEFT = pionmap[this.getX() - 1][this.getY()].getLast();
+        nextPosRIGHT = pionmap[this.getX() + 1][this.getY()].getLast();
+        nextPosDOWN = pionmap[this.getX()][this.getY() + 1].getLast();
+
+            for (int i = 0; i < tabNextPos.length; i++) {
+                if (tabNextPos[i] instanceof Empty) {
+                } else {
+                    found = true;
+                    indiceDirection = i;   //si deux joueurs à la même distance, il ira vers le dernier vu
+                }
+            }
+
+            if (found) {
+                switch (indiceDirection) {
+                    case 0:
+                        this.direction = UP;
+                        break;
+
+                    case 1:
+                        this.direction = LEFT;
+                        break;
+
+                    case 2:
+                        this.direction = RIGHT;
+                        break;
+
+                    case 3:
+                        this.direction = DOWN;
+                        break;
+                }
+
+            }
+        }
+        return this.direction;  //A CHANGER
     }
 
     @Override
